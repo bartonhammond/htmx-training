87b49b5121702ae44f626987022e2bbee76f6dcd
Barton Hammond <bartonhammond@gmail.com>
2024-12-31 18:53:02 -0600

    Basic retrieval and search

--- pb_hooks/index.pb.js
+++ pb_hooks/index.pb.js
@@ -1,14 +1,53 @@
 routerAdd("get", "/contacts", (e) => {
+  let search = e.request.url.query().get("q");
+  console.log(`search: ${search}`);
+  let records;
 
-
-    
-    const contacts=[{"id": 2, "first": "Carson", "last": "Gross", "phone": "123-456-7890", "email": "carson@example.comz", "errors": {}}, {"id": 3, "first": "", "last": "", "phone": "", "email": "joe@example.com", "errors": {}}, {"id": 4, "first": "", "last": "", "phone": "", "email": "carson@example.com", "errors": {}}]
-
-      const html = $template.loadFiles(
-        `${__hooks}/views/layout.html`,
-        `${__hooks}/views/index.html`,
-    ).render({
-	"contacts": contacts
+  records = arrayOf(
+    new DynamicModel({
+      // describe the shape of the data (used also as initial values)
+      id: "",
+      first: "",
+      last: "",
+      email: "",
+      phone: "",
     })
-    return e.html(200, html)
-})
+  );
+
+  if (!search) {
+    $app
+      .db()
+      .select("id", "first", "last", "email", "phone")
+      .from("contacts")
+      .limit(100)
+      .orderBy("first ASC", "last ASC")
+      .all(records);
+  } else {
+    $app
+      .db()
+      .select("id", "first", "last", "email", "phone")
+      .from("contacts")
+      .limit(100)
+      .where($dbx.like("first", search))
+      .orWhere($dbx.like("last", search))
+      .orWhere($dbx.like("email", search))
+      .orWhere($dbx.like("phone", search))
+      .orderBy("first ASC", "last ASC")
+      .all(records); // throw an error on db failurew
+  }
+  let contacts = records.map((x) => ({
+    id: x.id,
+    first: x.first,
+    last: x.last,
+    email: x.email,
+    phone: x.phone,
+  }));
+
+  const html = $template
+    .loadFiles(`${__hooks}/views/layout.html`, `${__hooks}/views/index.html`)
+    .render({
+      contacts: contacts,
+    });
+
+  return e.html(200, html);
+});
