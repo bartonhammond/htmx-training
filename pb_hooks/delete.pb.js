2369023781f249785b811b825c3796c2e8aee8c9
Barton Hammond <bartonhammond@gmail.com>
2025-01-06 10:31:23 -0600

    Delete

--- pb_hooks/delete.pb.js
+++ pb_hooks/delete.pb.js
@@ -1,10 +1,17 @@
 //View a particular contact
 routerAdd("delete", "/contact/{id}/", (e) => {
   let id = e.request.pathValue("id");
+  //where did the event get triggerd?  In the Edit page?
+  let deleteBtn = e.request.header.get('HX-Trigger')
 
-  console.log(`delete /contact/{id}id: ${id}`);
+  console.log(`delete /contact/{id}id: ${id} deleteBtn: ${deleteBtn}`);
 
   let record = $app.findRecordById("contacts", id);
   $app.delete(record);
-  e.redirect(303, "/contacts/?msg=Contact deleted!");
+
+  if (deleteBtn) {
+     e.response.header().set("Hx-Redirect", "/contacts")
+  } else {
+    return e.string(200, "")
+  }
 });
